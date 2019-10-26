b3da0d8f0aef12a0267367f0d9bfee6d6ab67cd6
nishu <nishantchauhanbvcoe@outlook.com>
2019-10-26 21:20:19 +0530

    production build

---
+++ build/precache-manifest.52b12ff735925b898a8db3f21ea5aa19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371dc9033a90cb7e1f7c9d17452b6be8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37f451efa1679fcd2299",
+    "url": "/static/css/main.a559014d.chunk.css"
+  },
+  {
+    "revision": "01507ab29d449b70d906",
+    "url": "/static/js/2.5275401f.chunk.js"
+  },
+  {
+    "revision": "37f451efa1679fcd2299",
+    "url": "/static/js/main.2bb3c7e0.chunk.js"
+  },
+  {
+    "revision": "044eb3fdf1b3a1a3cf83",
+    "url": "/static/js/runtime-main.fdec9e60.js"
+  }
+]);
